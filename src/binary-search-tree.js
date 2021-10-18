f5d41ed84f9d975dc9b8a7b483f6191979f00858
“Tanya <tanyazverevaa@gmail.com>
2021-10-18 19:19:06 +0300

    feat: add tree add method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,14 +8,33 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
+  #root = null
+
   root() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data)
+
+    if(this.#root == null){
+      this.#root = newNode
+    }
+    else{
+      this.insertNode(this.#root, newNode)
+    }
+  }
+
+  insertNode(node, newNode){
+    if(newNode.data < node.data){
+      if(node.left == null) node.left = newNode
+      else this.insertNode(node.left, newNode)
+    }
+    else{
+      if(node.right == null) node.right = newNode
+      else this.insertNode(node.right, newNode)
+    }
   }
 
   has(/* data */) {
